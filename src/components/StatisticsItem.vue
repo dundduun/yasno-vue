06028560727b02577ee62e4af7513eaedf09f1dc
dundduun <145420979+dundduun@users.noreply.github.com>
2024-03-18 16:10:49 +0300

    Create StatisticsItem compontent

---
+++ src/components/StatisticsItem.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps([
+    'id',
+    'title',
+    'definitionStart',
+    'definitionEnd'
+    ])
+</script>
+
+<template>
+    <span class="element-head">{{ title }}</span>
+    <span class="element-definition">{{ definitionStart }}</span>
+    <span class="element-definition">{{ definitionEnd }}</span>
+</template>
+
+<style scoped lang="scss">
+
+.element-head {
+    font-size: 35px;
+}
+
+.element-definition {
+    font-size: 20px;
+    color: #a4afba;
+}
+</style>
