f434cca5b9767eb95ff99703f63116bee833d091
dundduun <145420979+dundduun@users.noreply.github.com>
2024-03-24 15:46:35 +0300

    Create FooterLinksItem.vue component

---
+++ src/components/FooterLinksItem.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+
+defineProps<{
+    title: string,
+    links: {
+        name: string,
+        linkHref: string
+    }[]
+}>()
+
+</script>
+
+
+<template>
+    <div class="wrapper">
+        <h5>{{ title }}</h5>
+        <div class="links">
+            <a v-for="(link, index) in links" :href="link.linkHref" :key="index">{{ link.name }}</a>
+        </div>
+    </div>
+</template>
+
+
+<style scoped lang="scss">
+.wrapper {
+
+    h5 {
+        font-weight: normal;
+        font-size: 14px;
+    }
+
+    .links {
+        margin-top: 3px;
+        display: flex;
+        flex-direction: column;
+
+        a {
+            margin-top: 6px;
+            text-decoration: none;
+            color: #8497a6;
+            font-size: 14px;
+
+            &:hover {
+                color: #52626f;
+            }
+        }
+    }
+}
+</style>
