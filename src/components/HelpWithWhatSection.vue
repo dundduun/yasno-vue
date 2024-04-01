221a4dbd4159fb7aa2b266f656da93e56faa6590
dundduun <145420979+dundduun@users.noreply.github.com>
2024-04-01 22:40:15 +0300

    Start moving HelpWithWhatSection to grid
    Moving assumes only external section architecture
    to grid, not at all. Inner flex's retains.

--- src/components/HelpWithWhatSection.vue
+++ src/components/HelpWithWhatSection.vue
@@ -1,6 +1,4 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 
 <template>
@@ -9,55 +7,51 @@
             <h2 id="help-with-what">С чем <span>поможет</span> психолог?</h2>
 
             <ul>
-                <div class="three-items">
-                    <li>
-                        <img src="@/assets/images/help-with-one.svg" alt="Два человека в объятиях">
-                        <p>Наладить гармоничные<br>
-                            отношения с близкими людьми
-                        </p>
-                    </li>
+                <li>
+                    <img src="@/assets/images/help-with-one.svg" alt="Два человека в объятиях">
+                    <p>Наладить гармоничные<br>
+                        отношения с близкими людьми
+                    </p>
+                </li>
 
-                    <li>
-                        <img src="@/assets/images/help-with-two.svg"
-                            alt="Умиротворённая девушка, со сложенными в ладонях руками">
-                        <p>Справиться со стрессом,<br>
-                            тревогой и страхом
-                        </p>
-                    </li>
+                <li>
+                    <img src="@/assets/images/help-with-two.svg"
+                        alt="Умиротворённая девушка, со сложенными в ладонях руками">
+                    <p>Справиться со стрессом,<br>
+                        тревогой и страхом
+                    </p>
+                </li>
 
-                    <li>
-                        <img src="@/assets/images/help-with-three.svg" alt="Мужчина с радостью смотрит в зеркало">
-                        <p>Принять себя и повысить<br>
-                            самооценку
-                        </p>
-                    </li>
-                </div>
+                <li>
+                    <img src="@/assets/images/help-with-three.svg" alt="Мужчина с радостью смотрит в зеркало">
+                    <p>Принять себя и повысить<br>
+                        самооценку
+                    </p>
+                </li>
 
-                <div class="three-items">
-                    <li>
-                        <img src="@/assets/images/help-with-four.svg" alt="Девушка за столом держит руку лицевой стороной лодони, 
+                <li>
+                    <img src="@/assets/images/help-with-four.svg" alt="Девушка за столом держит руку лицевой стороной лодони, 
                             говоря нет без зазрения совести">
-                        <p>Научиться говорить «нет»<br>
-                            и отстаивать личные границы
-                        </p>
-                    </li>
+                    <p>Научиться говорить «нет»<br>
+                        и отстаивать личные границы
+                    </p>
+                </li>
 
-                    <li>
-                        <img src="@/assets/images/help-with-five.svg" alt="Мужчина держит в руках сердце с маленькими перекрёстными 
+                <li>
+                    <img src="@/assets/images/help-with-five.svg" alt="Мужчина держит в руках сердце с маленькими перекрёстными 
                             пластырями">
-                        <p>Пережить перемены: переезд,<br>
-                            расставание, потеря работы
-                        </p>
-                    </li>
+                    <p>Пережить перемены: переезд,<br>
+                        расставание, потеря работы
+                    </p>
+                </li>
 
-                    <li>
-                        <img src="@/assets/images/help-with-six.svg"
-                            alt="Девушка с радостью усердно трудится за компьютером">
-                        <p>Понять, почему на работе<br>
-                            проблемы и нет мотивации
-                        </p>
-                    </li>
-                </div>
+                <li>
+                    <img src="@/assets/images/help-with-six.svg"
+                        alt="Девушка с радостью усердно трудится за компьютером">
+                    <p>Понять, почему на работе<br>
+                        проблемы и нет мотивации
+                    </p>
+                </li>
             </ul>
 
             <button>Хочу обсудить свою ситуацию</button>
@@ -92,72 +86,66 @@
 
         ul {
             width: 100%;
-            display: flex;
-            flex-direction: column;
-            list-style-type: none;
+            display: grid;
+            grid-template-columns: 300px 300px 300px;
+            justify-content: space-evenly;
+            justify-items: center;
             align-items: center;
-            margin: 50px 0 0 0;
+            margin-top: 50px;
+            list-style-type: none;
             padding: 0;
 
-            .three-items {
-                width: 100%;
+            li {
+                height: 323px;
+                width: 304px;
                 display: flex;
-                flex-flow: row wrap;
-                justify-content: space-evenly;
-                align-items: baseline;
+                flex-direction: column;
+                align-items: center;
 
-                li {
-                    height: 323px;
-                    width: 304px;
-                    display: flex;
-                    flex-direction: column;
-                    align-items: center;
+                img {
+                    width: 208px;
+                    height: auto;
+                }
 
-                    img {
-                        width: 208px;
-                        height: auto;
-                    }
-
-                    p {
-                        font-size: 20px;
-                        margin-top: 25px;
-                        line-height: 150%;
-                        text-align: center;
-                    }
+                p {
+                    font-size: 20px;
+                    margin-top: 25px;
+                    line-height: 150%;
+                    text-align: center;
                 }
             }
         }
+    }
 
-        button {
-            height: 60px;
-            width: 390px;
-            font-size: 23px;
-            font-family: 'Rubik', sans-serif;
-            background-color: #45a7f5;
-            color: white;
-            border: 2px #45a7f5 solid;
-            border-radius: 8px;
-            margin-top: 10px;
-            cursor: pointer;
+    button {
+        height: 60px;
+        width: 390px;
+        font-size: 23px;
+        font-family: 'Rubik', sans-serif;
+        background-color: #45a7f5;
+        color: white;
+        border: 2px #45a7f5 solid;
+        border-radius: 8px;
+        margin-top: 10px;
+        cursor: pointer;
 
-            @media(hover: hover) {
-                transition: color 0.2s linear,
-                    background-color 0.2s linear;
+        @media(hover: hover) {
+            transition: color 0.2s linear,
+                background-color 0.2s linear;
 
-                &:hover {
-                    background: #f0f2f5;
-                    color: #45a7f5;
-                }
+            &:hover {
+                background: #f0f2f5;
+                color: #45a7f5;
             }
+        }
 
-            @media(hover: none) {
-                transition: 0.07s linear,
-                    background-color 0.07s linear;
+        @media(hover: none) {
+            transition: 0.07s linear,
+                background-color 0.07s linear;
 
-                &:active {
-                    background: #f0f2f5;
-                    color: #45a7f5;
-                }
+            &:active {
+                background: #f0f2f5;
+                color: #45a7f5;
             }
         }
     }
