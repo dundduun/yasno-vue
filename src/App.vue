b70676457d7b21341da77ddfb43daa771f337416
dundduun <145420979+dundduun@users.noreply.github.com>
2024-04-01 19:17:34 +0300

    Decompose App.vue by HelpWithWhatSection.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+import CoverSection from './components/CoverSection.vue'
 import StatisticsItem from './components/StatisticsItem.vue'
+import HelpWithWhatSection from './components/HelpWithWhatSection.vue'
 import Advantage from './components/Advantage.vue'
 import FooterLinksItem from './components/FooterLinksItem.vue'
-import CoverSection from './components/CoverSection.vue'
 
 const statisticsItems = [
   {
@@ -147,72 +148,12 @@ const currentYear = 1970 + Math.floor(Date.now() / 31_556_952_000);
   <div class="statistics">
     <ul>
       <li v-for="(item, index) in statisticsItems">
-        <StatisticsItem 
-        :key="index" 
-        :title="item.title" 
-        :definition="item.definition"
-        :width="item.width" />
+        <StatisticsItem :key="index" :title="item.title" :definition="item.definition" :width="item.width" />
       </li>
     </ul>
   </div>
 
-  <div class="section-2">
-    <div class="container">
-      <h2 id="help-with-what">С чем <span>поможет</span> психолог?</h2>
-
-      <ul>
-        <div class="three-items">
-          <li>
-            <img src="@/assets/images/help-with-one.svg" alt="Два человека в объятиях">
-            <p>Наладить гармоничные<br>
-              отношения с близкими людьми
-            </p>
-          </li>
-
-          <li>
-            <img src="@/assets/images/help-with-two.svg" alt="Умиротворённая девушка, со сложенными в ладонях руками">
-            <p>Справиться со стрессом,<br>
-              тревогой и страхом
-            </p>
-          </li>
-
-          <li>
-            <img src="@/assets/images/help-with-three.svg" alt="Мужчина с радостью смотрит в зеркало">
-            <p>Принять себя и повысить<br>
-              самооценку
-            </p>
-          </li>
-        </div>
-
-        <div class="three-items">
-          <li>
-            <img src="@/assets/images/help-with-four.svg"
-              alt="Девушка за столом держит руку лицевой стороной лодони, говоря нет без зазрения совести">
-            <p>Научиться говорить «нет»<br>
-              и отстаивать личные границы
-            </p>
-          </li>
-
-          <li>
-            <img src="@/assets/images/help-with-five.svg"
-              alt="Мужчина держит в руках сердце с маленькими перекрёстными пластырями">
-            <p>Пережить перемены: переезд,<br>
-              расставание, потеря работы
-            </p>
-          </li>
-
-          <li>
-            <img src="@/assets/images/help-with-six.svg" alt="Девушка с радостью усердно трудится за компьютером">
-            <p>Понять, почему на работе<br>
-              проблемы и нет мотивации
-            </p>
-          </li>
-        </div>
-      </ul>
-
-      <button>Хочу обсудить свою ситуацию</button>
-    </div>
-  </div>
+  <HelpWithWhatSection />
 
   <section class="service-advantages">
     <div class="container">
@@ -399,102 +340,6 @@ div {
   }
 }
 
-.section-2 {
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  padding: 70px 0 90px 0;
-  width: 100%;
-
-  .container {
-    width: 100%;
-    max-width: 1440px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    h2 {
-      font-size: 55px;
-      font-weight: 500;
-
-      span {
-        color: #42b2fc;
-      }
-    }
-
-    ul {
-      width: 100%;
-      display: flex;
-      flex-direction: column;
-      list-style-type: none;
-      align-items: center;
-      margin: 50px 0 0 0;
-      padding: 0;
-
-      .three-items {
-        width: 100%;
-        display: flex;
-        flex-flow: row wrap;
-        justify-content: space-evenly;
-        align-items: baseline;
-
-        li {
-          height: 323px;
-          width: 304px;
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-
-          img {
-            width: 208px;
-            height: auto;
-          }
-
-          p {
-            font-size: 20px;
-            margin-top: 25px;
-            line-height: 150%;
-            text-align: center;
-          }
-        }
-      }
-    }
-
-    button {
-      height: 60px;
-      width: 390px;
-      font-size: 23px;
-      font-family: 'Rubik', sans-serif;
-      background-color: #45a7f5;
-      color: white;
-      border: 2px #45a7f5 solid;
-      border-radius: 8px;
-      margin-top: 10px;
-      cursor: pointer;
-
-      @media(hover: hover) {
-        transition: color 0.2s linear,
-          background-color 0.2s linear;
-
-        &:hover {
-          background: #f0f2f5;
-          color: #45a7f5;
-        }
-      }
-
-      @media(hover: none) {
-        transition: 0.07s linear,
-          background-color 0.07s linear;
-
-        &:active {
-          background: #f0f2f5;
-          color: #45a7f5;
-        }
-      }
-    }
-  }
-}
-
 section.service-advantages {
   width: 100%;
   display: flex;
@@ -502,7 +347,6 @@ section.service-advantages {
   align-items: center;
   padding: 90px 0 90px 0;
   background-color: #f0f2f5;
-
 
   .container {
     width: 100%;
