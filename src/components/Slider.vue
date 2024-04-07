8cf73d469970de523c86fced93faaa80ba5e3385
dundduun <145420979+dundduun@users.noreply.github.com>
2024-04-07 15:34:11 +0300

    Create Slider.vue component

---
+++ src/components/Slider.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { gsap } from 'gsap'
+
+onMounted(() => {
+    let tl = gsap.timeline({ repeat: 5 });
+    tl.to('.slider-item', {duration: 0.5, y: -126, ease: 'power4' }, '>2.5');
+    tl.to('.slider-item', {duration: 0.5, y: -252, ease: 'power4' }, '>2.5');
+    tl.to('.slider-item', {duration: 0.5, y: -378, ease: 'power4' }, '>2.5');
+    tl.to('.slider-item', {duration: 0.5, y: -504 , ease: 'power4' }, '>2.5');
+    tl.to('.slider-item', {duration: 0.5, y: -630 , ease: 'power4' }, '>2.5');
+    tl.to('.slider-item', {duration: 0.5, y: 0, ease: 'power4'}, '>2.5')
+})
+</script>
+
+
+<template>
+    <ul class="slider-list">
+        <li class="slider-item">повысить качество жизни</li>
+        <li class="slider-item">справиться<br>
+            со стрессом</li>
+        <li class="slider-item">наладить отношения с близкими</li>
+        <li class="slider-item">найти своё призвание</li>
+        <li class="slider-item">принять себя</li>
+        <li class="slider-item">обрести спокойствие</li>
+    </ul>
+</template>
+
+<style scoped lang="scss">
+ul {
+    height: 126px;
+    overflow: hidden;
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+
+    li {
+        height: 126px;
+        font-size: 53px;
+        color: #42b2fc;
+        width: 100%;
+
+        // transform: translateY(-380px);
+    }
+}
+</style>
